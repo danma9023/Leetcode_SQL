8f68ba7d9fdd4097c18ac3de056048f2b345e63e
danma9023 <46980656+danma9023@users.noreply.github.com>
2021-06-25 16:00:25 -0600

    Update 534. Game Play Analysis III.py

--- 534. Game Play Analysis III.py
+++ 534. Game Play Analysis III.py
@@ -58,3 +58,12 @@
 select player_id, event_date,
 sum(games_played) over(partition by player_id order by event_date)  as games_played_so_far
 from Activity
+
+
+/* Write your T-SQL query statement below */
+select a1.player_id, a1.event_date,
+sum(a2.games_played) as games_played_so_far
+from Activity a1 left join Activity a2
+on a1.player_id= a2.player_id
+and a1.event_date >= a2.event_date
+group by a1.player_id, a1.event_date
