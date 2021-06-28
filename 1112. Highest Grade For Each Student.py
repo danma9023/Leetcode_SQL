d41c80402d2dbb2886bfabb34c799ca029fc3ed7
danma9023 <46980656+danma9023@users.noreply.github.com>
2021-06-28 17:01:10 -0600

    Create 1112. Highest Grade For Each Student.py

---
+++ 1112. Highest Grade For Each Student.py
@@ -0,0 +1,63 @@
+# 1112. Highest Grade For Each Student
+# Medium
+
+# 126
+
+# 4
+
+# Add to List
+
+# Share
+# SQL Schema
+# Table: Enrollments
+
+# +---------------+---------+
+# | Column Name   | Type    |
+# +---------------+---------+
+# | student_id    | int     |
+# | course_id     | int     |
+# | grade         | int     |
+# +---------------+---------+
+# (student_id, course_id) is the primary key of this table.
+
+# Write a SQL query to find the highest grade with its corresponding course for each student. In case of a tie, you should find the course with the smallest course_id. The output must be sorted by increasing student_id.
+
+# The query result format is in the following example:
+
+# Enrollments table:
+# +------------+-------------------+
+# | student_id | course_id | grade |
+# +------------+-----------+-------+
+# | 2          | 2         | 95    |
+# | 2          | 3         | 95    |
+# | 1          | 1         | 90    |
+# | 1          | 2         | 99    |
+# | 3          | 1         | 80    |
+# | 3          | 2         | 75    |
+# | 3          | 3         | 82    |
+# +------------+-----------+-------+
+
+# Result table:
+# +------------+-------------------+
+# | student_id | course_id | grade |
+# +------------+-----------+-------+
+# | 1          | 2         | 99    |
+# | 2          | 2         | 95    |
+# | 3          | 3         | 82    |
+# +------------+-----------+-------+
+
+
+
+
+# Write your MySQL query statement below
+
+select e.student_id, min(e.course_id) as course_id, e.grade
+from Enrollments e
+where (e.student_id, e.grade) in
+(
+select student_id, max(grade) as max_grade
+from Enrollments
+group by student_id
+) 
+group by e.student_id
+order by e.student_id
